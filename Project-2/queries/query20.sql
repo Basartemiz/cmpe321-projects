470e05204dbe7ec8889db617138fd096c923d8b2
Basartemiz <basar.temiz2004@gmail.com>
2025-03-30 17:23:24 +0300

    final files has been added

---
+++ Project-2/queries/query20.sql
@@ -0,0 +1,19 @@
+#query 20
+# This query first counts how many times each actor has appeared in each genre 
+# by joining 'Actors_and_Actresses', 'Movies', and 'Cast' and grouping by actor_id and movie.genre_id. 
+# Then, it groups again by actor_id to see how many distinct genres each actor has played in. 
+# A CASE expression labels whether an actor appeared in multiple genres. 
+# Finally, the results are ordered by actor surname in descending order.
+SELECT actor2.name, actor2.surname, COUNT(*) AS genre_count,
+       CASE WHEN COUNT(*) > 1 THEN 'True' ELSE 'False' END AS multiple_appearance
+FROM Actors_and_Actresses actor2,
+     (SELECT actor.actor_id, COUNT(*)
+      FROM Actors_and_Actresses actor, Movies movie, Cast cast
+      WHERE movie.movie_id = cast.movie_id
+        AND cast.actor_id = actor.actor_id
+      GROUP BY actor.actor_id, movie.genre_id
+      ORDER BY actor.actor_id
+     ) genres
+WHERE actor2.actor_id = genres.actor_id
+GROUP BY actor2.actor_id
+ORDER BY actor2.surname DESC;
