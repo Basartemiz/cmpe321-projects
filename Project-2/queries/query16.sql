639677b1b04636362ef04da6f5ce816d96a39b7a
Basartemiz <basar.temiz2004@gmail.com>
2025-04-14 20:58:54 +0300

    file 16 has been updated

--- Project-2/queries/query16.sql
+++ Project-2/queries/query16.sql
@@ -3,9 +3,16 @@
 # It filters where the actor_id and movie_id match up in both tables, 
 # then uses GROUP BY on actor_id to calculate the average rating of movies for each actor. 
 # The results are ordered by this average rating in descending order.
-SELECT actor.name, actor.surname, AVG(movie.rating) AS average_rating
-FROM Actors_and_Actresses actor, Movies movie, Cast cast
-WHERE movie.movie_id = cast.movie_id 
-  AND actor.actor_id = cast.actor_id
-GROUP BY actor.actor_id
-ORDER BY AVG(movie.rating) DESC;
+#used CTE in order to write clean code
+
+
+WITH uniqueMovies AS (
+  SELECT DISTINCT actor.actor_id, actor.name, actor.surname, movie.movie_id, movie.rating
+  FROM Actors_and_Actresses actor
+  JOIN Cast cast ON actor.actor_id = cast.actor_id
+  JOIN Movies movie ON movie.movie_id = cast.movie_id
+)
+SELECT actor_id, name, surname, AVG(rating) AS average_rating
+FROM UniqueMovies
+GROUP BY actor_id, name, surname
+ORDER BY average_rating DESC;
